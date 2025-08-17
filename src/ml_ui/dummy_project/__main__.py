aae8dd08ff0015a6ca8694a1ff9c7dab49bd18c2
Benjamin Ringdahl <benjaminringdahl@gmail.com>
2025-08-17 22:28:34 +0200

    simlpify __main__.py

--- src/ml_ui/dummy_project/__main__.py
+++ src/ml_ui/dummy_project/__main__.py
@@ -1,17 +1,7 @@
-import logging
 import subprocess
-import os
-from ml_ui.utils import setup_logging
 
 
 def main():
-    logger = logging.getLogger(__name__)
-    logger.info("Running docker according to the docker-compose.yaml file.")
-
-    env = os.environ.copy()
-    if "ML_HOMELAB_ROOT" not in env:
-        raise RuntimeError("ML_HOMELAB_ROOT is not set in the environment.")
-
     subprocess.run(
         [
             "docker",
@@ -20,13 +10,9 @@ def main():
             "docker-compose.dummy_project.yaml",
             "up",
             "--build",
-            "-d",
         ],
-        env=env,
-        check=True,
     )
 
 
 if __name__ == "__main__":
-    logger = setup_logging()
     main()
